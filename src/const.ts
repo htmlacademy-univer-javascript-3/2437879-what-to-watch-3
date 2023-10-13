8d77be20e14dd0b82a7b276d4d8132f01dc64a89
senyapekus <ksenya.pshenichnikova03@gmail.com>
2023-10-13 13:52:27 +0500

    Маршрутизация

--- src/const.ts
+++ src/const.ts
@@ -3,3 +3,9 @@ export const PromoFilm = {
   genre: 'Drama',
   year: 2014
 };
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <section className="user-page">
+      <h1>404. Page not found</h1>
+      <a href="/">Вернуться на главную</a>
+    </section>
+  );
+}
+
+export default NotFoundPage;
